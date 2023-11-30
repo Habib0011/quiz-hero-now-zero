8a2b0db7ea2a25e206978da6cdb19f04cb4ebe4e
Your Name01~Habib0011 <you@example.comhabiburrahman5236@gmail.com>
2023-11-30 09:43:38 +0600

    2nd commit

--- index.js
+++ index.js
@@ -22,9 +22,12 @@ startQuiz.addEventListener("click", () => {
   countDown.classList.add("flex");
 
   let x = setInterval(() => {
+    
     if (counterNum < 0) {
-      coutDown.classList.remove("flex");
-      coutDown.classList.add("hidden");
+      
+
+      countDown.classList.remove("flex");
+      countDown.classList.add("hidden");
       counterNum = 3;
       count = 0;
       timer = null;
@@ -46,7 +49,7 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
   displayQuiz(data);
 };
